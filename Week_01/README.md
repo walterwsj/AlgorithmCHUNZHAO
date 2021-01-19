d81595a46c41c39a10327ceff5ca11459ef06c0d
Walter <walterwsj@live.com>
2021-01-19 22:02:29 +0800

    New add homework content total 6 and Readme related

--- Week_01/README.md
+++ Week_01/README.md
@@ -1,1 +1,97 @@
 学习笔记
+删除排序数组中的重复项
+根据已排序特性，重复元素必相邻
+    定义两个指针，一个在前一个在后，如果后一个指针指向的元素跟前一个指针所指元素不同，就把
+这个不同的元素赋值给前一指针的下一位；如果相同前指针不动，后指针继续向前。直到后指针到达数组
+的末尾，前指针停止的地方正好就是非重复的最后一个元素
+    while q<len_nums:
+        if nums[p]!=nums[q]:
+            p+=1
+            nums[p]=nums[q]
+        else:
+            q+=1
+    return p+1
+
+旋转数组
+注意参数K的判断 
+    是否为数组长度的整数倍 如果是 直接返回原数组即可
+    如果是负数 正向循环长度-k的次数
+方法一:
+    字面意思 循环K次 数组所有元素全部依次向右移动即可
+    while k:
+        tmp=nums[-1]
+        for i in range(len_nums)[::-1]:
+            nums[i]=nums[i-1]
+        nums[0]=tmp
+        k-=1
+方法二:
+    字符串反转 全部反转->[:k-1]反转->[k:]反转
+    reverse(nums,0,len_nums-1)
+    reverse(nums,0,k-1)
+    reverse(nums,k,len_nums-1)
+
+方法3： 借用辅助空间
+    1. 循环队列索引思想
+        slave=copy.deepcopy(nums)
+        for i in range(len_nums):
+            nums[(i+k)%len_nums]=slave[i]
+    2. 已知结果拼结果
+        slave=nums[:len_nums-k]
+        # k 之前的元素删除
+        for i in range(len_nums-k):
+            del nums[0]
+        nums.extend(slave)
+
+合并两个有序数组
+方法1：典型归并排序思想 确定结果的末尾元素所以 依次从后向前赋值即可 缺点就是遍历的
+长度过长会影响时间复杂度
+方法2：以较短长度列表为外循环
+    i, j, k = m - 1, n - 1, m + n - 1
+    while j>0:
+        while i>=0 and nums2[j]<nums1[i]:
+            swap(nums1[i],nums1[k])
+            i-=1
+            k-=1
+        swap(nums2[j],nums1[k])
+        j-=1
+        k-=1
+
+两数之和
+方法1: 暴力 两层循环
+方法2: 利用字典 哈希特性 一次循环
+        for index,value in enumerate(nums):
+            rest=target-value
+            if rest in dict:
+                res.append(dict[rest],i)
+            else:
+                dict[rest]=i
+
+移动零
+方法一:两个指针 一个记录非零元素的下一个位置 另一个向前探索非零元素 如果有零出现 
+两指针会产生差值 交换位置即可
+方法二:设置一个index只记录非零元素。遍历结束index位置之后的元素全部赋值为零
+        for i in range(len_nums):
+            if nums[i]!=0:
+                nums[index]=nums[i]
+                index+=1
+        while index<len_nums:
+            nums[index]=0
+            index+=1
+        return nums
+
+加一
+方法一:
+数组转为数字，加1后返回（统计第一元素开始连续为零的个数）
+方法二:
+先在结果数组之前加一位 逢9为0 后一位加1
+    digits=[0]+digits
+    for i in range(len_digits)[::-1]:
+        if digits[i]==9:
+            digits[i]=0
+        else:
+            digits[i]+=1
+            break
+    if digits[0]==0:
+        return digits[1:]
+    else
+        return digits
---
+++ Week_01/homework.py
@@ -0,0 +1,225 @@
+import copy
+from typing import List
+
+"""
+给定一个排序数组，你需要在"原地"删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+"""
+
+
+def remove_duplicates(nums: List[int]) -> int:
+    if nums is None or len(nums) == 0:
+        return 0
+    former, latter, len_nums = 0, 1, len(nums)
+    while latter < len_nums:
+        if nums[former] != nums[latter]:
+            former += 1
+            nums[former] = nums[latter]
+        else:
+            latter += 1
+    return former + 1
+
+
+# test_nums = [1, 1, 2, 3]
+
+
+"""
+给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+"""
+
+
+def rotate_tmp_list(nums: List[int], k: int) -> list:
+    slave_list = copy.deepcopy(nums)
+    len_nums = len(nums)
+    for i in range(len_nums):
+        nums[(i + k) % len_nums] = slave_list[i]
+    return nums
+
+
+def rotate_tmp_list_2(nums: List[int], k: int) -> list:
+    len_num = len(nums)
+    k %= len_num
+    slave = nums[:len_num - k]
+    for i in range(len_num - k):
+        del nums[0]
+    nums.extend(slave)
+    return nums
+
+
+def rotate_reverse(nums: List[int], k: int) -> list:
+    len_nums = len(nums)
+    reverse(nums, 0, len_nums - 1)
+    reverse(nums, 0, k - 1)
+    reverse(nums, k, len_nums - 1)
+    return nums
+
+
+def reverse(nums, former, latter):
+    while former < latter:
+        nums[former], nums[latter] = nums[latter], nums[former]
+        former += 1
+        latter -= 1
+    return nums
+
+
+def rotate_regular(nums: List[int], k: int) -> list:
+    len_nums = len(nums)
+    if k % len_nums == 0:
+        return nums
+    while k:
+        tmp = nums[-1]
+        for i in range(len_nums - 1, -1, -1):
+            nums[i] = nums[i - 1]
+        nums[0] = tmp
+        k -= 1
+    return nums
+
+
+# test_nums = [1, 2, 3, 4, 5, 6, 7]
+
+
+"""
+给你两个有序整数数组nums1 和 nums2，请你将 nums2 合并到nums1中，使 nums1 成为一个有序数组。
+初始化nums1 和 nums2 的元素数量分别为m 和 n 。你可以假设nums1有足够的空间（空间大小等于m + n）来保存 nums2 中的元素。
+"""
+
+
+def merge(nums1: List[int], m: int, nums2: List[int], n: int):
+    i, j, k = m - 1, n - 1, m + n - 1
+    while i >= 0 and j >= 0:
+        if nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            k -= 1
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            k -= 1
+            j -= 1
+    while j >= 0:
+        nums1[k] = nums2[j]
+        k -= 1
+        j -= 1
+
+
+def merge_optimize(nums1: List[int], m: int, nums2: List[int], n: int):
+    i, j, k = m - 1, n - 1, m + n - 1
+    while j >= 0:
+        while i >= 0 and nums2[j] < nums1[i]:
+            nums1[i], nums1[k] = nums1[k], nums1[i]
+            i -= 1
+            k -= 1
+        nums1[k], nums2[j] = nums2[j], nums1[k]
+        k -= 1
+        j -= 1
+    return nums1
+
+
+# num1 = [1, 2, 3, 0, 0, 0]
+# m = 3
+# num2 = [2, 5, 6]
+# n = 3
+
+
+"""
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+"""
+
+
+def two_sum(nums: List[int], target: int):
+    res, len_nums = [], len(nums)
+    for i in range(len_nums - 1):
+        for j in range(i + 1, len_nums):
+            if nums[i] + nums[j] == target:
+                res.append([i, j])
+    return res
+
+
+def two_sum_optimize(nums: List[int], target: int):
+    res, len_nums = {}, len(nums)
+    for index, value in enumerate(nums):
+        rest_part = target - value
+        if rest_part in res:
+            return [res[rest_part], index]
+        else:
+            res[rest_part] = index
+    return res
+
+
+"""
+给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+"""
+
+
+def move_zeroes(nums: List[int]):
+    j, len_nums = 0, len(nums)
+    for i in range(len_nums):
+        if nums[i] != 0:
+            if i != j:
+                nums[i], nums[j] = nums[j], nums[i]
+            j += 1
+    return nums
+
+
+def move_zeroes_optimize(nums: List[int]):
+    index, len_nums = 0, len(nums)
+    for i in range(len_nums):
+        if nums[i] != 0:
+            nums[index] = nums[i]
+            index += 1
+    while index < len_nums:
+        nums[index] = 0
+        index += 1
+    return nums
+
+
+"""
+给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+最高位数字存放在数组的首位，数组中每个元素只存储单个数字。
+你可以假设除了整数0之外，这个整数不会以零开头。
+"""
+
+
+def plus_one(digits: List[int]):
+    len_main, len_slave = len(digits), len(digits)
+    index = 0
+    result = 0
+    while len_main > 0:
+        result += digits[index] * 10 ** (len_main - 1)
+        index += 1
+        len_main -= 1
+    len_tmp_result = len(str(result + 1))
+    if len_tmp_result == len_slave:
+        return [int(i) for i in str(result + 1)]
+    else:
+        count = len_slave - len_tmp_result
+        new_result = [0] * count
+        new_result += [int(i) for i in str(result + 1)]
+        return new_result
+
+
+def plus_one_opi(digits: List[int]):
+    len_digits, count = len(digits), -1
+    for i in range(len_digits):
+        if digits[i] != 0:
+            break
+        count += 1
+    res = int("".join([str(i) for i in digits])) + 1
+    return [0] * count + [int(x) for x in list(str(res))]
+
+
+def plus_one_opi_1(digits: List[int]):
+    len_digits, digits = len(digits), [0] + digits
+    for i in range(len_digits)[::-1]:
+        if digits[i] == 9:
+            digits[i] = 0
+        else:
+            digits[i] += 1
+            break
+    if digits[0] == 0:
+        return digits[1:]
+    else:
+        return digits
+
+
+a = [1, 2, 3]
+print(remove_duplicates(a))
