57cd2a35b3d0fb8d741e37d8412e677ed13eb453
Walter <walterwsj@live.com>
2021-02-24 16:57:10 +0800

    Add one 1 excises

--- Week_05/homework.py
+++ Week_05/homework.py
@@ -1,6 +1,9 @@
+import collections
 from typing import List
 
 from collections import Counter
+
+from urllib3.connectionpool import xrange
 
 """
 爬楼梯
@@ -160,3 +163,53 @@ class Trie:
                 return False
             node = node[char]
         return True
+
+
+"""
+单词搜索
+"""
+
+end_of_word = "#"
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+class Solution:
+    def __init__(self):
+        self.m = 0
+        self.n = 0
+        self.result = None
+
+    def find_words(self, board: List[List[str]], words: List[str]) -> List[str]:
+        if not board or not board[0]:
+            return []
+        if not words:
+            return []
+        self.result = set()
+        root = collections.defaultdict()
+
+        for word in words:
+            node = root
+            for char in word:
+                node = node.setdefault(char, {})
+            node[end_of_word] = end_of_word
+
+        self.m, self.n = len(board), len(board[0])
+        for i in xrange(self.m):
+            for j in xrange(self.n):
+                if board[i][j] in root:
+                    self._dfs(board, i, j, "", root)
+
+        return list(self.result)
+
+    def _dfs(self, board, i, j, current_word, current_dict):
+        current_word += board[i][j]
+        current_dict = current_dict[board[i][j]]
+        if end_of_word in current_dict:
+            self.result.add(current_word)
+        tmp, board[i][j] = board[i][j], '@'
+        for k in xrange(4):
+            x, y = i + dx[k], j + dy[k]
+            if 0 <= x < self.m and 0 <= y < self.n and board[x][y] != '@' and board[x][y] in current_dict:
+                self._dfs(board, x, y, current_word, current_dict)
+        board[i][j] = tmp
